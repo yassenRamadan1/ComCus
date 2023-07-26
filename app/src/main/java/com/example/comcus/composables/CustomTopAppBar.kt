36c58cb6090153f3844caab6b745f0de43876931
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2023-07-27 00:36:37 +0300

    add icon and app bar

--- app/src/main/java/com/example/comcus/composables/CustomTopAppBar.kt
+++ app/src/main/java/com/example/comcus/composables/CustomTopAppBar.kt
@@ -1,8 +1,49 @@
 package com.example.comcus.composables
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.comcus.R
 
 @Composable
-fun CustomTopAppBar(){
+fun CustomTopAppBar(
+    modifier: Modifier=Modifier,
+    height: Dp =60.dp,
+    color: Color =Color.White,
+    paddingValues: PaddingValues= PaddingValues(16.dp),
+    showLeftICon:Boolean = true,
+    showRightICon:Boolean = false,
+    leftICon:Painter = painterResource(id = R.drawable.baseline_arrow_back_24),
+    rightICon:Painter = painterResource(id = R.drawable.baseline_arrow_back_24),
 
+    ){
+    Row(
+        modifier
+            .fillMaxWidth()
+            .height(height)
+            .background(color),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+        ){
+        Icon(painter = leftICon, contentDescription = "")
+
+    }
+
+}
+@Preview
+@Composable
+fun TopAppBarPreview(){
+    CustomTopAppBar()
 }
