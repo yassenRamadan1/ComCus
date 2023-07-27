1d215a8e28310b2946cf418b6ffa1cabc3f69293
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2023-07-27 03:01:53 +0300

    add text and app bar

--- app/src/main/java/com/example/comcus/composables/CustomTopAppBar.kt
+++ app/src/main/java/com/example/comcus/composables/CustomTopAppBar.kt
@@ -2,10 +2,13 @@ package com.example.comcus.composables
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.comcus.R
+import java.nio.file.WatchEvent
 
 @Composable
 fun CustomTopAppBar(
@@ -27,7 +31,8 @@ fun CustomTopAppBar(
     showRightICon:Boolean = false,
     leftICon:Painter = painterResource(id = R.drawable.baseline_arrow_back_24),
     rightICon:Painter = painterResource(id = R.drawable.baseline_arrow_back_24),
-
+    text:String ="Main Activity",
+    textColor:Color= Color.Black
     ){
     Row(
         modifier
@@ -37,8 +42,17 @@ fun CustomTopAppBar(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
         ){
-        Icon(painter = leftICon, contentDescription = "")
-
+        Box(modifier =Modifier.padding(start = paddingValues.calculateTopPadding())){
+            if (showLeftICon)
+            Icon(painter = leftICon, contentDescription = "")
+        }
+        Box(modifier =Modifier.padding(start = paddingValues.calculateTopPadding())){
+            Text(text = text, modifier = Modifier,color=textColor)
+        }
+        Box(modifier =Modifier.padding(end = paddingValues.calculateTopPadding())){
+            if (showRightICon)
+            Icon(painter = rightICon, contentDescription = "")
+        }
     }
 
 }
